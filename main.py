ded16036c700188a3895607bbe53133d93abe5cd
Cristian <me@cristian.io>
2024-06-26 08:53:03 -0700

    added catetorization

--- main.py
+++ main.py
@@ -15,7 +15,21 @@ from lunchable.models import TransactionObject, TransactionUpdateObject
 
 already_sent_transactions: List[int] = []
 
-async def send_transaction_message(context: ContextTypes.DEFAULT_TYPE, transaction: TransactionObject, chat_id: str) -> None:
+def get_buttons(transaction_id: int, plaid=True, skip=True, mark_reviewed=True, categorize=True):
+    buttons = []
+    if categorize:
+        buttons.append(InlineKeyboardButton("Categorize", callback_data=f"categorize_{transaction_id}"))
+    if plaid:
+        buttons.append(InlineKeyboardButton("Dump plaid details", callback_data=f"plaid_{transaction_id}"))
+    if skip:
+        buttons.append(InlineKeyboardButton("Skip", callback_data=f"skip_{transaction_id}"))
+    if mark_reviewed:
+        buttons.append(InlineKeyboardButton("Mark as Reviewed", callback_data=f"review_{transaction_id}"))
+    # max two buttons per row
+    buttons = [buttons[i:i + 2] for i in range(0, len(buttons), 2)]
+    return buttons
+
+async def send_transaction_message(context: ContextTypes.DEFAULT_TYPE, transaction: TransactionObject, chat_id, message_id=None) -> None:
     # Format the amount with monospaced font
     formatted_amount = f"`${transaction.amount:.2f}`"
 
@@ -27,7 +41,7 @@ async def send_transaction_message(context: ContextTypes.DEFAULT_TYPE, transacti
         pst_date_time = date_time.astimezone(pst_tz)
         formatted_date_time = pst_date_time.strftime("%Y-%m-%d %I:%M:%S %p PST")
     else:
-        formatted_date_time = "N/A"
+        formatted_date_time = transaction.plaid_metadata.get('date')
 
     # Get category and category group
     category = transaction.category_name or "Uncategorized"
@@ -36,55 +50,29 @@ async def send_transaction_message(context: ContextTypes.DEFAULT_TYPE, transacti
     # Get account display name
     account_name = transaction.plaid_account_display_name or "N/A"
 
-    message = f"*{category_group}*\n\n"
+    # split the category group into two: the first emoji and the rest of the string
+    emoji, rest = category_group.split(" ", 1)
+
+    message = f"{emoji} #*{rest.replace(" ", "_")}*\n\n"
     message += f"*Payee:* {transaction.payee}\n"
     message += f"*Amount:* {formatted_amount}\n"
     message += f"*Date/Time:* {formatted_date_time}\n"
-    message += f"*Category:* {category} \n"
-    message += f"*Account:* {account_name}\n"
+    message += f"*Category:* #{category} \n"
+    message += f"*Account:* #{account_name}\n"
 
 
-    keyboard = [
-        [InlineKeyboardButton("Mark as Reviewed", callback_data=f"review_{transaction.id}")]
-    ]
+    keyboard = get_buttons(transaction.id)
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    # Check if category image is available
-    category_image_url = transaction.plaid_metadata.get('personal_finance_category_icon_url')
-    if category_image_url and False: # this is disabled for now
-        try:
-            # Download the image
-            response = requests.get(category_image_url)
-            response.raise_for_status()
-            image_data = io.BytesIO(response.content)
-            image_data.name = "category_image.png"
-
-            photo = InputMediaPhoto(
-                media=image_data,
-                caption=message,
-                parse_mode=ParseMode.MARKDOWN
-            )
-
-            # Send document with thumbnail
-            await context.bot.send_photo(
-                chat_id=chat_id,
-                photo=photo,
-                # caption=message,
-                # parse_mode=ParseMode.MARKDOWN,
-                reply_markup=reply_markup
-            )
-        except Exception as e:
-            print("Failed to send doc: ", e)
-            
-            await context.bot.send_photo(
-                chat_id=chat_id,
-                photo=category_image_url,
-                caption=message,
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=reply_markup
-            )
+    if message_id:
+        await context.bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=message_id,
+            text=message,
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=reply_markup
+        )
     else:
-        # Send message without image
         await context.bot.send_message(
             chat_id=chat_id,
             text=message,
@@ -94,10 +82,6 @@ async def send_transaction_message(context: ContextTypes.DEFAULT_TYPE, transacti
 
 def setup_handlers(config):
     application = Application.builder().token(config["TELEGRAM_BOT_TOKEN"]).build()
-
-    # Store necessary data in bot_data
-    # application.bot_data["LUNCH_MONEY_TOKEN"] = config["LUNCH_MONEY_TOKEN"]
-    # application.bot_data["TELEGRAM_CHAT_ID"] = config["TELEGRAM_CHAT_ID"]
 
     lunch = LunchMoney(access_token=config["LUNCH_MONEY_TOKEN"])
 
@@ -119,23 +103,87 @@ def setup_handlers(config):
                 print('Ignoring already sent transaction: ', transaction.id)
                 continue
             already_sent_transactions.append(transaction.id)
-            await send_transaction_message(context, transaction, config["TELEGRAM_CHAT_ID"])
+            await send_transaction_message(context, transaction, '378659027')
 
         return transactions
 
     async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         query = update.callback_query
+        print("query.data", query.data)
+
+        chat_id = query.message.chat.id
         await query.answer()
 
+        if query.data.startswith("skip"):
+            await query.edit_message_reply_markup(reply_markup=None)
+            return
+        
         transaction_id = int(query.data.split("_")[1])
         
-        try:
-            lunch.update_transaction(transaction_id, TransactionUpdateObject(status='cleared'))
+        if query.data.startswith("cancelCategorization"):
+            await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(get_buttons(transaction_id)))
+            return
 
-            # Remove the inline keyboard (button)
-            await query.edit_message_reply_markup(reply_markup=None)
-        except Exception as e:
-            await query.edit_message_text(text=f"Error updating transaction: {str(e)}")
+        if query.data.startswith("categorize"):
+            transaction = lunch.get_transaction(transaction_id)
+            categories = lunch.get_categories()
+            category_buttons = []
+            for category in categories:
+                if category.group_id is None:
+                    category_buttons.append(InlineKeyboardButton(category.name, callback_data=f"subcategorize_{transaction_id}_{category.id}"))
+            category_buttons = [category_buttons[i:i + 3] for i in range(0, len(category_buttons), 3)]
+            category_buttons.append([InlineKeyboardButton("Cancel", callback_data=f"cancelCategorization_{transaction_id}")])
+            await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(category_buttons))
+            return
+        
+        if query.data.startswith("subcategorize"):
+            transaction_id, category_id = query.data.split("_")[1:]
+            subcategories = lunch.get_categories()
+            subcategory_buttons = []
+            for subcategory in subcategories:
+                if str(subcategory.group_id) == str(category_id):
+                    subcategory_buttons.append(InlineKeyboardButton(subcategory.name, callback_data=f"applyCategory_{transaction_id}_{subcategory.id}"))
+            subcategory_buttons = [subcategory_buttons[i:i + 3] for i in range(0, len(subcategory_buttons), 3)]
+            subcategory_buttons.append([InlineKeyboardButton("Cancel", callback_data=f"cancelCategorization_{transaction_id}")])
+            
+            await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(subcategory_buttons))
+            return
+        
+        if query.data.startswith("applyCategory"):
+            transaction_id, category_id = query.data.split("_")[1:]
+            lunch.update_transaction(transaction_id, TransactionUpdateObject(category_id=category_id))
+            updated_transaction = lunch.get_transaction(transaction_id)
+            await send_transaction_message(context, updated_transaction, chat_id, query.message.message_id)
+            return
+        
+        if query.data.startswith("plaid"):
+            transaction = lunch.get_transaction(transaction_id)
+            plaid_metadata = transaction.plaid_metadata
+            plaid_details = f"*Plaid Metadata*\n\n"
+            for key, value in plaid_metadata.items():
+                if value is not None:
+                    plaid_details += f"*{key}:* `{value}`\n"
+            await context.bot.send_message(
+                chat_id=chat_id,
+                text=plaid_details,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_to_message_id=query.message.message_id,
+            )
+
+            await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(get_buttons(transaction_id, plaid=False)))
+            return
+        
+        if query.data.startswith("review"):
+            try:
+                lunch.update_transaction(transaction_id, TransactionUpdateObject(status='cleared'))
+
+                # Remove the inline keyboard (button)
+                await query.edit_message_reply_markup(reply_markup=None)
+            except Exception as e:
+                await query.edit_message_text(text=f"Error updating transaction: {str(e)}")
+            return
+        
+        await context.bot.send_message(chat_id=chat_id, text="Unknown command")
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("check_transactions", check_transactions_manual))
@@ -153,7 +201,6 @@ def load_config():
     return {
         "LUNCH_MONEY_TOKEN": os.getenv("LUNCH_MONEY_TOKEN"),
         "TELEGRAM_BOT_TOKEN": os.getenv("TELEGRAM_BOT_TOKEN"),
-        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID")
     }
 
 
